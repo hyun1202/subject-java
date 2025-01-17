32f04fa6a2cf60ace809524356539d0826e6ef4a
hyun <apfhd0257@naver.com>
2025-01-17 13:21:54 +0900

    first commit

---
+++ src/main/java/com/example/subjectjava/SubjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.subjectjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/subjectjava/SubjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subjectjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
