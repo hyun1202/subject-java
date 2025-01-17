d9f0d244e10546d2a742f4190fae6171e7e50f9c
hyun <apfhd0257@naver.com>
2025-01-17 20:19:47 +0900

    test: 테스트 완료 후 롤백 되도록 변경

--- src/test/java/com/example/subjectjava/user/UserRepositoryTest.java
+++ src/test/java/com/example/subjectjava/user/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -14,6 +15,7 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
+    @Transactional
     void findUsername() {
         String username = "test12";
 
